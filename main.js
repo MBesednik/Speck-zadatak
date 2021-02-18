17224c81c06f758a782cb4bf6b34bd1464d6f046
MBesednik <m.besednik69@gmail.com>
2021-02-18 23:15:52 +0100

    Popravljeno uklanjanje

--- main.js
+++ main.js
@@ -7,16 +7,16 @@ function dodajProizvod() {
     li.appendChild(document.createTextNode(proizvod.value));
     ul.appendChild(li);
 }
+
 let integer = 0;
 // uklanjanje proizvoda
 function ukloniProizvod() {
     let ul = document.getElementById("listaProizvoda");
-    // let proizvod = document.getElementById("proizvod");
-    // let item = document.getElementById(proizvod.value);
-    // ul.removeChild(item);    
-    ul.remove('listaProizvoda');
+    let proizvod = document.getElementById("proizvod");
+    let item = document.getElementById(proizvod.value);
+    ul.removeChild(item);
+    window.location.reload(true);
 }
-
 
 // Brojac
 let uvećaj = document.getElementById('dodaj');
@@ -24,11 +24,12 @@ let umanji = document.getElementById('ukloni');
 
 let int = document.getElementById('broj');
 
-
 uvećaj.addEventListener('click', function () {
     integer += 1;
     int.innerHTML = integer;
 })
+
+// Vrati na 0
 umanji.addEventListener('click', function () {
     integer = 0;
     int.innerHTML = integer;
